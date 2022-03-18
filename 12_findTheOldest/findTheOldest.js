163f96157e73108e2fe6691e0884cbd65853eab8
JorgeLg3 <jorgelopezgamarra@gmail.com>
2022-03-18 19:54:53 +0100

    Exercises 8 to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ordered = people.sort(function(a, b){
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    });
+    return ordered[0];
 };
 
 // Do not edit below this line
